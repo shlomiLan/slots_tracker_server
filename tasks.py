2cbc30f1ea241f40b60679cf3ccf638f2892fa4c
shlomiLan <shlomilanton@gmail.com>
2018-06-28 00:13:06 +0300

    Don't use flask run to run the server

--- tasks.py
+++ tasks.py
@@ -1,10 +1,6 @@
 from invoke import task
-import os
 
 
 @task
 def run(c):
-    os.environ['FLASK_APP'] = 'server'
-    os.environ['FLASK_ENV'] = 'development'
-
-    c.run("flask run")
+    c.run("python server.py")
