9a7ed7cce87b90924baf8415612d66d775da16cc
shlomiLan <shlomilanton@gmail.com>
2018-06-28 02:33:14 +0300

    update test for creating new expense

--- slots_tracker/test_api.py
+++ slots_tracker/test_api.py
@@ -1,9 +1,13 @@
-import pytest
+# system modules
+import datetime
 
-from expense import create_new_expense
+from expense import create
 from conf import PayMethods
 
 
+# Chnage test to not use the real DB
 def test_create_new_expense():
-    response = create_new_expense(amount=200, desc='Random stuff', pay_method=PayMethods.Visa)
-    assert response.get('code') == 201
+    response = create(dict(amount=200, desc='Random stuff',
+                           pay_method=PayMethods.Visa.value, timestamp=datetime.datetime.utcnow()))
+    # We got a success code
+    assert response[1] == 201
