a8821ff71a172b3915bbe79df72bb321abeed93b
shlomiLan <shlomilanton@gmail.com>
2018-06-28 01:58:38 +0300

    Fix functions to create, read one and read enture list of expenses

--- slots_tracker/expense.py
+++ slots_tracker/expense.py
@@ -1,31 +1,38 @@
-import datetime
+# 3rd party modules
+from flask import abort
+from bson.json_util import dumps
+from bson.objectid import ObjectId
+from bson.errors import InvalidId
 
-from db import mongo_client
-
-
-def expense_data_from_url_parameters(parameters):
-    amount = parameters.get('amount')
-    desc = parameters.get('desc')
-    pay_method = parameters.get('pay_method')
-    date = parameters.get('date', datetime.datetime.today())
-
-    return amount, desc, pay_method, date
+from db import db
 
 
-def create_new_expense(db, amount, desc, pay_method, date):
-    expense = dict(amount=amount, desc=desc, pay_method=pay_method, date=date)
+def create(expense):
     expense_id = db.expenses.insert_one(expense).inserted_id
-
-    return dict(code=201) if expense_id else dict(code=400)
-
-
-def create_expense():
-    # TODO: find better way to do that
-    # amount, desc, pay_method, date = expense_data_from_url_parameters(request.args)
-    # create_new_expense(mongo_client, amount, desc, pay_method, date)
-    return 'Expense Page post'
+    return read_one(expense_id), 201
 
 
-def read():
-    # expenses_q = mongo_client.expenses.find({'amount': 200})
-    return 'Expense Page get'
+def read_all():
+    expenses = db.expenses.find()
+    return dumps(expenses)
+
+
+def read_one(expense_id):
+    """
+    This function responds to a request for /api/expense/{expenses_id}
+    with one matching expense from expenses list
+    :param expense_id: id of the expense to find
+    :return:           expense matching id
+    """
+    try:
+        # Does it a valid ID
+        object_id = ObjectId(expense_id)
+        # Does the expense exist in the DB
+        expense = db.expenses.find_one({'_id': object_id})
+        if expense:
+            return dumps(expense)
+        # otherwise, raise an error
+        else:
+            abort(404, 'Expense with id {} not found'.format(expense_id))
+    except InvalidId:
+        abort(404, '{} is not a valid object ID'.format(expense_id))
