8bea2c1a34738f0be368517c7af0c771369d72ca
shlomiLan <shlomilanton@gmail.com>
2018-07-01 20:41:05 +0300

    Restructure project to separate server and client

--- server/server.py
+++ server/server.py
@@ -4,7 +4,7 @@ import connexion
 app = connexion.App(__name__, specification_dir='./')
 
 # Read the swagger.yml file to configure the endpoints
-app.add_api('swagger.yml')
+# app.add_api('swagger.yml')
 
 
 @app.route('/')
