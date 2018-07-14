0569cd071924d59ff675292f3bb7954c226d9372
shlomiLan <shlomilanton@gmail.com>
2018-07-15 01:42:10 +0300

    More changes form using 'flask-mongoengine'

--- server/expense.py
+++ server/expense.py
@@ -5,20 +5,21 @@ import datetime
 # 3rd party modules
 from flask.views import MethodView
 from flask import request
-from mongoengine import *
+
+from server import db
 
 from server.board import Board
 from server.utils import convert_to_object_id
 
 
 # Find way to add data with migration script
-class PayMethods(Document):
-    name = StringField(required=True, max_length=200)
+class PayMethods(db.Document):
+    name = db.StringField(required=True, max_length=200)
 
 
-class Expense(Document):
-    amount = IntField()
-    descreption = StringField(required=True, max_length=200)
+class Expense(db.Document):
+    amount = db.IntField()
+    descreption = db.StringField(required=True, max_length=200)
     # pay_method = ReferenceField(PayMethods, required=True)
     # timestamp = DateTimeField(default=datetime.datetime.utcnow)
     # board = ReferenceField(Board, required=True)
@@ -30,11 +31,9 @@ class ExpenseAPI(MethodView):
             return Expense.objects.to_json()
         else:
             object_id = convert_to_object_id(expense_id)
-            try:
-                # Does the expense exist in the DB
-                return Expense.objects.get(id=object_id).to_json()
-            except DoesNotExist:
-                abort(404, 'Expense with id {} not found'.format(object_id))
+            expense = Expense.objects.get_or_404(id=object_id)
+
+            return expense.to_json()
 
     def post(self):
         expense_data = json.loads(request.data)
@@ -48,7 +47,7 @@ class ExpenseAPI(MethodView):
         object_id = convert_to_object_id(expense_id)
         expense_data = json.loads(request.data)
         # Get and updaete the expense
-        expense = Expense.objects.get(id=object_id)
+        expense = Expense.objects.get_or_404(id=object_id)
         expense.update(**expense_data)
         #  Reload the expense with the updated data
         return expense.reload().to_json()
