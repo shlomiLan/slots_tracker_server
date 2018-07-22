141634e83a16efb675f260971cb54bf23b946a36
shlomiLan <shlomilanton@gmail.com>
2018-07-22 10:32:38 +0300

    Bring 'flask-monogoengine' back

--- server/__init__.py
+++ server/__init__.py
@@ -2,18 +2,18 @@ import os
 
 from flask import Flask
 from flask_cors import CORS
-from mongoengine import *
+from flask_mongoengine import MongoEngine
 
 
 app = Flask(__name__)
 CORS(app)
 
-print(os.environ.get('FLASK_TEST'))
-if (os.environ.get('FLASK_TEST') == 'true'):
+app.logger.info('Is flask test: {}'.format(os.environ.get('FLASK_TEST')))
+if os.environ.get('FLASK_TEST') == 'true':
     app.config['MONGODB_SETTINGS'] = dict(db='slots_tracker_test')
 else:
     app.config['MONGODB_SETTINGS'] = dict(db='slots_tracker')
 
-connect(app.config['MONGODB_SETTINGS'].get('db'))
+db = MongoEngine(app)
 
 import server.views  # noqa
