79caa5be7a93d946b47dcedd1cdb54567cfda023
shlomiLan <shlomilanton@gmail.com>
2018-07-23 12:53:51 +0300

    Change data output and input for better performance in client and minor syntax changes

--- server/expense.py
+++ server/expense.py
@@ -1,12 +1,10 @@
-# system modules
-import json
 import datetime
 
-# 3rd party modules
 from flask.views import MethodView
 from flask import request
 from mongoengine import *
 from mongoengine.errors import NotUniqueError
+from bson import json_util
 
 from server.db import BaseDocument
 from server.board import Board
@@ -14,26 +12,24 @@ from server.utils import convert_to_object_id
 
 
 class BaseAPI(MethodView):
-    def get(self, id):
-        if id is None:
+    def get(self, obj_id):
+        if obj_id is None:
             return self.api_class.objects.to_json()
         else:
-            object_id = convert_to_object_id(id)
+            object_id = convert_to_object_id(obj_id)
             instance = self.api_class.objects.get_or_404(id=object_id)
             return instance.to_json()
 
-    def post(self):
-        data = json.loads(request.data)
+    def post(self, data):
         self.find_and_convert_ids(data)
         return self.api_class(**data).save().to_json(), 201
 
-    def delete(self, id):
+    def delete(self, obj_id):
         # delete a single user
         pass
 
-    def put(self, id):
-        object_id = convert_to_object_id(id)
-        data = json.loads(request.data)
+    def put(self, obj_id, data):
+        object_id = convert_to_object_id(obj_id)
         self.find_and_convert_ids(data)
 
         # Get and updaete the expense
@@ -42,7 +38,8 @@ class BaseAPI(MethodView):
         #  Reload the expense with the updated data
         return instance.reload().to_json()
 
-    def find_and_convert_ids(self, data):
+    @staticmethod
+    def find_and_convert_ids(data):
         for k, v in data.items():
             if isinstance(v, dict):
                 for k2, v2 in v.items():
@@ -58,15 +55,19 @@ class PayMethods(BaseDocument):
 class PayMethodsAPI(BaseAPI):
     api_class = PayMethods
 
-    def post(self):
+    def post(self, data=None):
         try:
-            return super(PayMethodsAPI, self).post()
+            if not data:
+                data = json_util.loads(request.data)
+            return super(PayMethodsAPI, self).post(data)
         except NotUniqueError:
             return 'Name value must be unique', 400
 
-    def put(self, id):
+    def put(self, obj_id, data=None):
         try:
-            return super(PayMethodsAPI, self).put(id)
+            if not data:
+                data = json_util.loads(request.data)
+            return super(PayMethodsAPI, self).put(obj_id, data)
         except NotUniqueError:
             return 'Name value must be unique', 400
 
@@ -81,3 +82,32 @@ class Expense(BaseDocument):
 
 class ExpenseAPI(BaseAPI):
     api_class = Expense
+
+    def get(self, obj_id):
+        data = json_util.loads(super(ExpenseAPI, self).get(obj_id))
+        if isinstance(data, dict):
+            data = [data]
+
+        for item in data:
+            pay_method_data = PayMethods.objects.get(id=item.get('pay_method')).to_json()
+            item['pay_method'] = json_util.loads(pay_method_data)
+
+        return json_util.dumps(data[0] if len(data) == 1 else data)
+
+    def post(self, data=None):
+        data = self.pay_method_json_to_object(data)
+        return super(ExpenseAPI, self).post(data)
+
+    def put(self, obj_id, data=None):
+        data = self.pay_method_json_to_object(data)
+        return super(ExpenseAPI, self).put(obj_id, data)
+
+    @staticmethod
+    def pay_method_json_to_object(data):
+        if not data:
+            data = json_util.loads(request.data)
+
+        if 'pay_method' in data:
+            data['pay_method'] = convert_to_object_id(data.get('pay_method').get('_id'))
+
+        return data
--- server/views.py
+++ server/views.py
@@ -8,5 +8,5 @@ def home_page():
     return 'Index Page3'
 
 
-register_api(ExpenseAPI,    'expense_api',     '/expenses/',    pk='id')  # noqa
-register_api(PayMethodsAPI, 'pay_methods_api', '/pay_methods/', pk='id')
+register_api(ExpenseAPI,    'expense_api',     '/expenses/',    pk='obj_id')  # noqa
+register_api(PayMethodsAPI, 'pay_methods_api', '/pay_methods/', pk='obj_id')
