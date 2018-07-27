53150effbef26b53c1255c5472bb17d9d3727dc7
shlomiLan <shlomilanton@gmail.com>
2018-07-27 21:42:04 +0300

    Use 'flask-cors' instead of CSRF_ENABLED = True

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     TESTING = False
-    CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
     MONGODB_SETTINGS = dict(db='slots_tracker')
 
