9a8ee4f7018dee6dd1fd8ba28eb8d9c19227e2dd
shlomiLan <shlomilanton@gmail.com>
2018-08-10 18:25:58 +0300

    Improve pytest setup and teardown

--- slots_tracker_server/tests/conftest.py
+++ slots_tracker_server/tests/conftest.py
@@ -1,10 +1,12 @@
+import datetime
+
 import pytest
 
 from slots_tracker_server import app as flask_app
 from slots_tracker_server.expense import Expense, PayMethods
 
 
-@pytest.fixture
+@pytest.fixture(scope="session", autouse=True)
 def client():
     flask_client = flask_app.test_client()
     # Clean the DB
@@ -13,4 +15,12 @@ def client():
 
     # create fake PayMethods
     PayMethods('Visa').save()
+    # Create fake Expense
+    Expense(amount=200, description='Random stuff', pay_method=PayMethods.objects().first(),
+            timestamp=datetime.datetime.utcnow).save()
+
     yield flask_client
+
+    # Clean the DB
+    Expense.objects.delete()
+    PayMethods.objects.delete()
