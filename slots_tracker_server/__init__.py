ba0f558a55b4c5f907ade341d782d3ccd5299cd6
shlomiLan <shlomilanton@gmail.com>
2018-09-16 00:28:42 +0300

    Fix DB init

--- slots_tracker_server/__init__.py
+++ slots_tracker_server/__init__.py
@@ -8,10 +8,8 @@ from slots_tracker_server import commands
 
 app = Flask(__name__)
 CORS(app)
-connect(
-    dict(host=os.environ.get('DB_HOST'), port=int(str(os.environ.get('DB_PORT'))),
-         username=os.environ.get('DB_USERNAME'), password=os.environ.get('DB_PASS'), name=os.environ.get('DB_NAME')))
-
+connect(host=os.environ.get('DB_HOST'), port=int(str(os.environ.get('DB_PORT'))),
+        username=os.environ.get('DB_USERNAME'), password=os.environ.get('DB_PASS'), name=os.environ.get('DB_NAME'))
 
 import slots_tracker_server.views  # noqa
 
