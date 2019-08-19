89fcb24f7663d77f7830b99816e7d6141bff7f55
shlomiLan <shlomilanton@gmail.com>
2019-08-19 23:49:57 +0300

    Add `retryWrites` flag

--- slots_tracker_server/__init__.py
+++ slots_tracker_server/__init__.py
@@ -10,7 +10,8 @@ from slots_tracker_server import commands
 app = Flask(__name__)
 CORS(app)
 connect(host=os.environ.get('DB_HOST'), port=int(str(os.environ.get('DB_PORT'))),
-        username=os.environ.get('DB_USERNAME'), password=os.environ.get('DB_PASS'), name=os.environ.get('DB_NAME'))
+        username=os.environ.get('DB_USERNAME'), password=os.environ.get('DB_PASS'), name=os.environ.get('DB_NAME'),
+        retryWrites=False)
 sentry = Sentry(app, dsn=os.environ.get('SENTRY_DSN'))
 
 import slots_tracker_server.views  # noqa
