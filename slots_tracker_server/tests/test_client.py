ec7e4301ba43a22793997bc04e7ab05b37d7b8fd
shlomiLan <shlomilanton@gmail.com>
2020-05-09 18:53:08 +0300

    Try fix chrome in tests

--- slots_tracker_server/tests/test_client.py
+++ slots_tracker_server/tests/test_client.py
@@ -4,6 +4,7 @@ from helium import start_chrome, kill_browser
 
 def test_home_page_load(client):
     driver = start_chrome(headless=True)
+    print(driver.__dict__)
     # A Selenium API:
     # driver.execute_script("alert('Hi!');")
     kill_browser()
