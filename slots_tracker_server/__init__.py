c472e9ad3f4203464015cd43e781819031930c3e
Shlomi Lanton <shlomiLan@users.noreply.github.com>
2020-09-06 20:58:08 +0300

    Migrate db (#160)
    * atlas support
    
    * atlas support - 2
    
    * fix typo
    
    * add packages
    
    * Fix local mongo
    
    * missing paramter
    
    * prints
    
    * cleanup

--- slots_tracker_server/__init__.py
+++ slots_tracker_server/__init__.py
@@ -7,9 +7,15 @@ from raven.contrib.flask import Sentry
 
 app = Flask(__name__)
 CORS(app)
-connect(host=os.environ.get('DB_HOST'), port=int(str(os.environ.get('DB_PORT'))),
-        username=os.environ.get('DB_USERNAME'), password=os.environ.get('DB_PASS'), name=os.environ.get('DB_NAME'),
-        retryWrites=False)
+
+DB_HOST = os.environ.get('DB_HOST')
+DB_NAME = os.environ.get('DB_NAME')
+DB_USERNAME = os.environ.get('DB_USERNAME')
+DB_PASSWORD = os.environ.get('DB_PASS')
+DB_URI_TEMPLATE = os.environ['DB_URI_TEMPLATE']
+DB_URI = DB_URI_TEMPLATE.format(DB_HOST=DB_HOST, DB_NAME=DB_NAME, DB_USERNAME=DB_USERNAME, DB_PASSWORD=DB_PASSWORD)
+connect(host=DB_URI)
+
 sentry = Sentry(app, dsn=os.environ.get('SENTRY_DSN'))
 
 import slots_tracker_server.views  # noqa
